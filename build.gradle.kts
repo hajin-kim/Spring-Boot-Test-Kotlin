a4473bf36bce0e51da2289ace4e49500cdce2bd0
Hajin Kim <kimhajin@yonsei.ac.kr>
2022-07-12 11:55:06 +0900

    Make a variable for kotlin version in Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,9 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint-idea") version "10.3.0"
 
     val kotlinVersion = "1.6.21"
-    kotlin("jvm") version "1.6.21"	// Kotlin 으로 작성된 프로젝트의 컴파일 및 다른 task 를 위한 플러그인
-    kotlin("plugin.spring") version "1.6.21"
-    kotlin("plugin.jpa") version "1.6.21" // // Entity NoArgsConstructor 어노테이션이 없어도 되게 해줌
+    kotlin("jvm") version kotlinVersion	// Kotlin 으로 작성된 프로젝트의 컴파일 및 다른 task 를 위한 플러그인
+    kotlin("plugin.spring") version kotlinVersion
+    kotlin("plugin.jpa") version kotlinVersion // // Entity NoArgsConstructor 어노테이션이 없어도 되게 해줌
     kotlin("kapt") version kotlinVersion
 }
 
